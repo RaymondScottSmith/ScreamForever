0da25344ef73ab4a7b6ad33250ae421edaf5fd1b
RaymondScottSmith <rayscottsmith1@gmail.com>
2025-06-26 15:02:51 -0500

    added sofa and radio stuff

--- Assets/Scripts/Interactables/Radio.cs
+++ Assets/Scripts/Interactables/Radio.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class Radio : Interact
 {
+    public AudioSource musicSource;
+    public AudioSource scaresSource;
+
+    public AudioClip baseSong;
+    public AudioClip policeChatter;
+    public AudioClip speech;
+    public AudioClip screams;
+
+    private bool playing = true;
+    private float musicVolume;
+    private void Awake()
+    {
+        musicVolume = musicSource.volume;
+    }
     public override void Interaction()
     {
         switch (currentIter)
         {
             default:
+                if (playing)
+                {
+                    musicSource.volume = 0f;
+                    playing = false;
+                }
+                else
+                {
+                    playing = true;
+                    musicSource.volume = musicVolume;
+                }
                 readyToAdvance = true;
                 CanvasManager.Instance.InterruptDisplay(baseInteraction);
                 break;
@@ -24,7 +48,11 @@ public class Radio : Interact
         {
             case 0:
                 break;
+            case 7:
+                break;
             default:
+                playing = true;
+                musicSource.volume = musicVolume;
                 readyToAdvance = true;
                 break;
         }
